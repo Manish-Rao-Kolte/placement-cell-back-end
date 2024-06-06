09d257375040b378dea43874b181fe9e168ce2f3
Manish-Rao-Kolte <138801683+Manish-Rao-Kolte@users.noreply.github.com>
2024-06-06 20:28:18 +0530

    fix bugs.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { connectDB } from "./config/mongoose.js";
 const port = process.env.PORT;
 
 connectDB()
-  .then(() => {
-    app.on("error", (err) => {
-      console.log("error", err);
-    });
-    app.listen(port, () => {
-      console.log(`server is running on port: ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log(`Mongo DB connection failure: ${err}`);
-  });
+   .then(() => {
+      app.on("error", (err) => {
+         console.log("error", err);
+      });
+      app.listen(port, () => {
+         console.log(`server is running on port: ${port}`);
+      });
+   })
+   .catch((err) => {
+      console.log(`Mongo DB connection failure: ${err}`);
+   });
